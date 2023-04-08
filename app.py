f96385df02d6d79be8aaac131c3d6f613d9c79ae
mikeqiu2011 <mikeqiu2011@163.com>
2023-04-08 15:29:23 +0800

    async done

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template, request
 import openai
 import os
+import asyncio
+import concurrent.futures
 
 app = Flask(__name__)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+
 history = []
+
+def chat_wrapper(message):
+    prompt = {'role': 'user',
+              'content': message}
+    history.append(prompt)
+
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=history
+    )
+
+    reply = response.choices[0].message.content
+    history.append({'role': 'assistant',
+                    'content': reply})
+
+    return {"message": reply}
+
+async def async_chat(message):
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(executor, chat_wrapper, message)
+    return response
+
 
 
 @app.route("/")
@@ -14,20 +40,13 @@ def home():
 
 
 @app.route("/chat", methods=["POST"])
-def chat():
+async def chat():
     message = request.json["message"]
-    prompt = f"User: {message}\nAI:"
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    reply = response.choices[0].text.strip()
-    history.append((message, reply))
-    return {"message": reply}
+
+    result = await async_chat(message)
+    # result = chat_wrapper(message)
+    return result
+
 
 
 app.run(debug=True)
